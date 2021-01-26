17d332eb1eefb113daa259b143cff7b5f0ba879c
Robert Pearce <me@robertwpearce.com>
2021-01-26 17:30:32 -0500

    include more linux systems

--- flake.nix
+++ flake.nix
@@ -5,8 +5,12 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, flake-utils, nixpkgs }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ]
-      (system:
+    flake-utils.lib.eachSystem [
+      "x86_64-linux"
+      "x86_64-darwin"
+      "i686-linux"
+      "aarch64-linux"
+    ] (system:
         let pkgs = nixpkgs.legacyPackages.${system}; in
         {
           checks = {};
