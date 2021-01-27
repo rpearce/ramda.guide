e3d25d14a48dd8183add01854f2679785a87873a
Robert Pearce <me@robertwpearce.com>
2021-01-26 21:45:09 -0500

    simplify flake systems

--- flake.nix
+++ flake.nix
@@ -5,28 +5,21 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, flake-utils, nixpkgs }:
-    flake-utils.lib.eachSystem [
-      "x86_64-linux"
-      "x86_64-darwin"
-      "i686-linux"
-      "aarch64-linux"
-    ] (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in rec {
-          packages = flake-utils.lib.flattenTree {
-            mdbook = pkgs.mdbook;
-          };
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in rec {
+        packages = flake-utils.lib.flattenTree {
+          mdbook = pkgs.mdbook;
+        };
 
-          defaultPackage = pkgs.mdbook;
+        defaultPackage = pkgs.mdbook;
 
-          apps.book = flake-utils.lib.mkApp {
-            drv = pkgs.mdbook;
-          };
+        apps.book = flake-utils.lib.mkApp {
+          drv = pkgs.mdbook;
+        };
 
-          defaultApp = apps.book;
+        defaultApp = apps.book;
 
-          devShell = import ./shell.nix { inherit pkgs; };
-        }
-      );
+        devShell = import ./shell.nix { inherit pkgs; };
+      }
+    );
 }
